d9b654dadb2e6c5e173b1e58f36b1f2a6a8eefa5
sophierenshaw <renshaw.sophie@gmail.com>
2015-03-30 15:01:44 +0100

    Nova and glance development started

--- nova_dev.py
+++ nova_dev.py
@@ -5,9 +5,30 @@
 
 from os import environ as env
 from keystoneclient.v2_0 import client
+import glanceclient.v2.client as glclient
+#from credentials import get_nova_credentials_v2
+from novaclient.client import Client
 keystone = client.Client(auth_url=env['OS_AUTH_URL'],#keystone service endpoint for authorisation
 			username=env['OS_USERNAME'], #username for auth
 			password=env['OS_PASSWORD'], #password for auth
 			tenant_name=env['OS_TENANT_NAME']) #tenant name
 
-print keystone.auth_token
+#print keystone.auth_token
+glance_endpoint = keystone.service_catalog.url_for(service_type ='image')
+glance = glclient.Client(glance_endpoint, token=keystone.auth_token)
+images = glance.images.list()
+print list(images)
+
+def get_nova_credentials_v2():
+	d = {}
+	d['version'] = '2'
+	d['username'] = env['OS_USERNAME']
+	d['api_key'] = env['OS_PASSWORD']
+	d['auth_url'] = env['OS_AUTH_URL']
+	d['project_id'] = env['OS_TENANT_NAME']
+	return d
+
+credentials = get_nova_credentials_v2()
+nova_client = Client(**credentials)
+
+print(nova_client.servers.list())
