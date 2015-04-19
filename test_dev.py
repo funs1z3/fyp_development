77bf28526a651e101c678e98db96a23400af0ca7
sophierenshaw <renshaw.sophie@gmail.com>
2015-04-19 15:35:00 +0000

    added comments

--- test_dev.py
+++ test_dev.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
-#Author: SOphie Renshaw
+#Author: Sophie Renshaw
 #Date: 10/04/2015
 #Script to test the Nova Lab Exercise Solution
 
-with open('nova_dev.py', 'r') as file1:
-	with open('nova_solution.py', 'r') as file2:
-		same = set(file1).intersection(file2)
+with open('nova_dev.py', 'r') as file1: #opening the sample solution file
+	with open('nova_solution.py', 'r') as file2: #opening student solution file
+		same = set(file1).intersection(file2) #get lines that are the same
 
-same.discard('\n')
+same.discard('\n') #discards blank lines
 
-with open('compare.txt', 'w') as file_out:
-	for line in same:
-		file_out.write(line)
+with open('compare.txt', 'w') as file_out: #opens the compare.txt file
+	for line in same: #for each line that is the same in both files
+		file_out.write(line) #write the line to compare.txt
 
-solution_count = len(open('nova_dev.py').readlines())
-count = len(open('compare.txt').readlines())
-i = float(count) / solution_count
-marks = i*100
-print marks, "%"
+solution_count = len(open('nova_dev.py').readlines()) #counts number of lines in sample solution
+count = len(open('compare.txt').readlines()) #counts number of lines in the compare.txt file
+i = float(count) / solution_count #divides lines in compare by lines in solution 
+marks = i*100 #gets percentage
+print marks, "%" #returns to user
